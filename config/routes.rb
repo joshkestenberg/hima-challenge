77decb64f79215a111ae25f5996e1d892ee39911
joshkestenberg <joshkestenberg@gmail.com>
2019-01-15 14:19:34 -0500

    changed index route to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :clock_events, only: [:index, :create, :update]
+  resources :clock_events, only: [:create, :update]
   get 'clock_events/home', to: 'clock_events#home'
   get 'clock_events/punch_clock', to: 'clock_events#punch_clock', as: 'punch_clock'
+  get 'admin', to: 'clock_events#index'
 
   root to: 'clock_events#home'
 end
